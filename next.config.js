9d335a5002850d0e75204e6d591425b209da98c2
Reskyppo <reskybeen@gmail.com>
2020-08-14 19:46:00 +0700

    init commit

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
-const withPlugins  = require("next-compose-plugins")
-const withCss      = require("@zeit/next-css")
-const withReactSVG = require("next-react-svg")
-const withImages   = require("next-images")
-const path         = require("path")
+const withPlugins = require("next-compose-plugins");
+const withCss = require("@zeit/next-css");
+const withReactSvg = require("next-react-svg");
+const withImages = require("next-images");
+
+const path = require("path");
 
 module.exports = withPlugins([
-    withCss({}),
-    withImages({}),
-    withReactSVG({
-        include: path.resolve(__dirname, "./public/images"),
-        webpack(config, options){
-            return config;
-        }
-    })
-])
+  withCss({}),
+  withImages({}),
+  withReactSvg({
+    include: path.resolve(__dirname, "./public/images"),
+    webpack(config, options) {
+      return config;
+    },
+  }),
+]);
