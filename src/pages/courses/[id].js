7f55800d79b9b4d3385970981ee79f0cfd0dfb10
Reskyppo <reskybeen@gmail.com>
2020-08-19 21:35:07 +0700

    add description

--- src/pages/courses/[id].js
+++ src/pages/courses/[id].js
@@ -124,21 +124,35 @@ function DetailsCourse({ data }) {
                   </div>
                   <h5 className="text-2xl text-teal-500 whitespace-no-wrap mr-4">
                     {data?.type === "free" ? (
-                      "free"
+                      "Free"
                     ) : (
                       <span>Rp {formatThousand(data?.price ?? 0)}</span>
                     )}
                   </h5>
                   <a
-                    href={`${process.env.NEXT_PUBLIC_MEMBERPAGE_URL}?/joined/${data.id}`}
+                    href={`${process.env.NEXT_PUBLIC_MEMBERPAGE_URL}/joined/${data.id}`}
                     target="_blank"
                     rel="noopener noreferrer"
                     className="bg-orange-500 hover:bg-orange-400 transition-all duration-200 focus:outline-none shadow-inner text-white px-6 py-3 whitespace-no-wrap"
-                  ></a>
+                  >
+                    {data?.type === "free" ? "Enroll Now" : "Buy Now"}
+                  </a>
                 </div>
               </div>
             </div>
           </CSSTransition>
+        </div>
+        <div className="mx-auto mt-8 w-3/4">
+          <div className="w-3/4">
+            <section>
+              <h6 className="font-medium text-gray-900 text-2xl mb-4">
+                About <span className="text-teal-500">Courses</span>
+              </h6>
+              <p className="text-gray-600 tex-lg leading-relaxed mb-3">
+                {data?.description ?? "No Description Found"}
+              </p>
+            </section>
+          </div>
         </div>
       </section>
       <div className="" style={{ height: 2000 }}></div>
